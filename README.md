efba31767e733769f9a0eedc0ab94c57f027e1cc
iqbalwaldan <iqbalwaldan0510@gmail.com>
2022-05-12 19:57:43 +0700

    Firebase Flutter

---
+++ README.md
@@ -0,0 +1,15 @@
+# Firebase Flutter
+
+Nama : Moh. Iqbal Waldan
+
+Kelas : MI-2F
+
+NIM : 203171039
+
+## Halaman Login
+
+![Login](https://user-images.githubusercontent.com/69692712/168079504-23274363-2ac5-464c-a97c-3b8786186ad0.jpeg)
+
+## Halaman Home
+
+![Home](https://user-images.githubusercontent.com/69692712/168079512-8340d5ad-7c23-4649-bf98-b3db34ec2101.jpeg)
---
+++ lib/login_page.dart
@@ -0,0 +1,180 @@
+// Moh. Iqbal Waldan
+// MI-2F
+// 2031710139
+
+import 'package:firebase_flutter/second_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_flutter/sign_in.dart';
+import 'package:firebase_flutter/first_screen.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController(text: "");
+  TextEditingController passwordController = TextEditingController(text: "");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Moh. Iqbal Waldan (2031710139)"),
+      ),
+      body: ListView(children: [
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: 20),
+            const FlutterLogo(size: 150),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30, right: 30, top: 20),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                controller: emailController,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(
+                  left: 30, right: 30, top: 10, bottom: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                controller: passwordController,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+              child: ElevatedButton(
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(
+                      color: Color.fromARGB(240, 255, 255, 255),
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    await AuthSign.signIn(
+                            emailController.text, passwordController.text)
+                        .then((result) {
+                      if (result != null) {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return SecondScreen();
+                            },
+                          ),
+                        );
+                      }
+                    });
+                  }),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+              child: ElevatedButton(
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Color.fromARGB(240, 255, 255, 255),
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    await AuthSign.signUp(
+                            emailController.text, passwordController.text)
+                        .then((result) {
+                      if (result != null) {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return SecondScreen();
+                            },
+                          ),
+                        );
+                      }
+                    });
+                  }),
+            ),
+            Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: const Text(
+                  "or",
+                  style: TextStyle(fontSize: 20),
+                )),
+            _signInButton(),
+          ],
+        ),
+      ]),
+    );
+  }
+
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.blue,
+      onPressed: () {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return FirstScreen();
+                },
+              ),
+            );
+          }
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: const BorderSide(color: Colors.blue),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            // Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.blue,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
